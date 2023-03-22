c0b83c554ddfe781e9bc489ed02ac99505b158f4
Edona Krosa Islami <MY_NAME@example.com>
2023-03-22 22:55:02 +0100

    Add input in gallery to search for employees by firstname and lastname

--- frontend/src/component/EmployeeGallery.tsx
+++ frontend/src/component/EmployeeGallery.tsx
@@ -1,18 +1,41 @@
 import {Employee} from "../model/Employee";
 import EmployeeCard from "./EmployeeCard";
 import "../styling/EmployeeGallery.css";
+import {ChangeEvent, useState} from "react";
 
 type Props = {
     employees: Employee[]
 }
 export default function EmployeeGallery(props: Props) {
-    const employeeCard = props.employees.map((employee) => {
+
+    const [filter, setFilter] = useState("")
+
+    const filteredList = props.employees.filter((employee) =>
+        employee
+            .firstName
+            .toLowerCase()
+            .includes(filter.toLowerCase())
+        ||
+        employee
+            .lastName
+            .toLowerCase()
+            .includes(filter.toLowerCase())
+    );
+
+    function handleFilterChange(event: ChangeEvent<HTMLInputElement>) {
+        setFilter(event.target.value)
+    }
+
+    const employeeCards = filteredList.map((employee) => {
             return <EmployeeCard key={employee.id} employee={employee}/>
         }
     )
     return (
-        <section className={"employee-gallery"}>
-            {employeeCard.length>0 ? employeeCard : "No employees yet"}
-        </section>
+        <>
+            <input type={"text"} onChange={handleFilterChange} placeholder={"Search"}/>
+            <section className={"employee-gallery"}>
+                {employeeCards.length > 0 ? employeeCards : "No employees yet"}
+            </section>
+        </>
     )
 }
