971873fbe14378b395c9633fcd45812d7ef8f1f0
Edona Krosa Islami <MY_NAME@example.com>
2023-03-22 22:56:08 +0100

    Fix employee card

--- frontend/src/component/EmployeeCard.tsx
+++ frontend/src/component/EmployeeCard.tsx
@@ -1,15 +1,19 @@
 import {Employee} from "../model/Employee";
 import "../styling/EmployeeCard.css";
+import moment from "moment";
 
 type EmployeeCardProps = {
     employee:Employee
 }
 export default function EmployeeCard(props:EmployeeCardProps) {
+
     return(
+        <>
         <div className={"employee-card"}>
-            <p>{props.employee.firstName}</p>
-            <p>{props.employee.lastName}</p>
-            <p>{props.employee.id}</p>
+            <p>{props.employee.firstName} {props.employee.lastName} </p>
+            <p>ID: {props.employee.id}</p>
+            Added: {moment(props.employee.added).format("YYYY-MM-DD HH:mm")}
         </div>
+        </>
     )
 }
