eca77ffdfaba98458c54e8de7a4f798a7dbcc2e7
Edona Krosa Islami <MY_NAME@example.com>
2023-03-28 17:38:57 +0200

    Add Csrf endpoint and test

---
+++ backend/src/test/java/com/github/edona94/controller/CsrfControllerTest.java
@@ -0,0 +1,26 @@
+package com.github.edona94.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CsrfControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getCsrfToken_whenGetRequest_thenReceiveCSRF_Cookie() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/csrf"))
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("XSRF-TOKEN"));
+    }
+}
