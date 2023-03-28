d468080fd51e54d998f0576db5f53c48e089225d
Edona Krosa Islami <MY_NAME@example.com>
2023-03-29 00:23:29 +0200

    Add MongoUserController and signup endpoint

---
+++ backend/src/main/java/com/github/edona94/controller/MongoUserController.java
@@ -0,0 +1,24 @@
+package com.github.edona94.controller;
+
+import com.github.edona94.model.MongoUserRequest;
+import com.github.edona94.model.MongoUserResponse;
+import com.github.edona94.service.MongoUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class MongoUserController {
+    private final MongoUserDetailsService mongoUserDetailsService;
+    @PostMapping
+    public MongoUserResponse signup(@RequestBody MongoUserRequest user) {
+        return mongoUserDetailsService.signup(user);
+    }
+    @GetMapping("/me")
+    public MongoUserResponse getMe(Principal principal) {
+        return mongoUserDetailsService.getMe(principal);
+    }
+}
