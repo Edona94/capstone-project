7874ac3fd955c551704310eb47d95b3f900051e5
Edona Krosa Islami <MY_NAME@example.com>
2023-03-29 15:14:04 +0200

    Add findAllByOrderByAddedDesc() in EmployeeRepository

--- backend/src/main/java/com/github/edona94/repository/EmployeeRepository.java
+++ backend/src/main/java/com/github/edona94/repository/EmployeeRepository.java
@@ -4,6 +4,9 @@ import com.github.edona94.model.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee,String> {
+    List<Employee> findAllByOrderByAddedDesc();
 }
