8ea956be6a668e2d7a945cd960a2660390bcdec3
Edona Krosa Islami <MY_NAME@example.com>
2023-03-30 17:18:22 +0200

    Improve styling

--- frontend/src/component/SignUpPage.tsx
+++ frontend/src/component/SignUpPage.tsx
@@ -6,6 +6,7 @@ export default function SignUpPage() {
     return (
         <Layout>
             <div style={{padding: "5rem 0"}}>
+                <h2 className={"sign-in-h2"}>Sign Up</h2>
                 <SignForm action={"sign-up"}/>
             </div>
         </Layout>
