4a609f5db2d79ba0c972f6b5974307c73d2aba88
Edona Krosa Islami <MY_NAME@example.com>
2023-03-30 15:09:26 +0200

    Add log out in navigation when a user is signed in

--- frontend/src/component/Navigation.tsx
+++ frontend/src/component/Navigation.tsx
@@ -1,9 +1,25 @@
 import '../styling/Header.css'
+import axios from "axios";
+import {useLocation} from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function Navigation() {
-    return(
+    const user = useAuth(false)
+    const location = useLocation()
+
+    function handleLogOutClick() {
+        axios.post("/api/users/logout").then(() => {
+            window.sessionStorage.setItem(
+                "signInRedirect",
+                location.pathname || "/"
+            );
+            window.location.href = "/sign-in";
+        });
+    }
+
+    return !user ? <> </> : (
         <nav className={"nav"}>
-        <button>Log out</button>
+            <button onClick={handleLogOutClick}>Log out</button>
         </nav>
     )
 }
