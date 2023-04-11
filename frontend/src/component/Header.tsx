cbbed299f0dd5f0bd1053faff52d7273cdbf90a5
Edona Krosa Islami <MY_NAME@example.com>
2023-04-11 10:52:58 +0200

    Add Home and Statistics in Navigation

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -4,10 +4,7 @@ import '../styling/Header.css'
 export default function Header() {
     return(
         <header className={"header"}>
-            <div>
-            <h2>HR<span>works</span>
-            <Navigation/></h2>
-            </div>
+            <Navigation/>
         </header>
     )
 }
--- frontend/src/component/Navigation.tsx
+++ frontend/src/component/Navigation.tsx
@@ -1,6 +1,6 @@
 import '../styling/Header.css'
 import axios from "axios";
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 export default function Navigation() {
@@ -17,9 +17,24 @@ export default function Navigation() {
         });
     }
 
-    return !user ? <> </> : (
-        <nav className={"nav"}>
-            <button onClick={handleLogOutClick}>Log out</button>
-        </nav>
+    return (
+        <div className={"nav"}>
+            <div className={"navigation-block"}>
+                <h2>HR<span>works</span></h2>
+            </div>
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <div className={"navigation-block"}>
+                {user ?
+                    <>
+                        <Link to={"/"}><i className="fa fa-fw fa-home"></i>Home</Link>
+                        <Link to={"/chart"}>Statistics</Link>
+                        <Link to={'#'} onClick={handleLogOutClick}>Log out</Link>
+                    </>
+                    :
+                    null
+                }
+            </div>
+        </div>
     )
 }
