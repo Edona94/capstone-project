18a6981dfc65f35cc6bbaf98e3a3cee07ac84a81
Edona Krosa Islami <MY_NAME@example.com>
2023-04-13 15:54:13 +0200

    Add CVDeletionFailedException

---
+++ backend/src/main/java/com/github/edona94/exception/CVDeletionFailedException.java
@@ -0,0 +1,8 @@
+package com.github.edona94.exception;
+
+public class CVDeletionFailedException extends RuntimeException  {
+
+    public CVDeletionFailedException(String message) {
+        super(message);
+    }
+}
